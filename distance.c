b663b632c3ebffb5ff7694b6b324032a090452c5
Ahad253 <ansariahad0318@gmail.com>
2025-02-16 15:04:27 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -5,24 +5,32 @@ struct distance{
 	float inches;
 };
 int main (){
-	int i=0;
-	struct distance distance[2];
+	struct distance s[2];
+	int totalfeet,i=0,x=0;
+	float totalinches;
 	for (i=0;i<2;i++){
-	printf("enter the distance[%d]:\n",i+1);
-	printf("\nfeets :");
-	scanf("%d",&distance[i].feet);
-	printf("\ninches:");
-	scanf("%f",&distance[i].inches);
-}
-    int x=distance[0].feet+distance[1].feet;                                          
-    float y=distance[0].inches+distance[1].inches;
-    int z;
-    if(y>=12){
-    	x+=y/12;
-    	z=fmod(y,12);
-    	y=z;
+	printf("enter the distance %d:\n",i+1);
+	printf("enter feet:");
+	scanf("%d",&s[i].feet);
+	if (s[i].feet<0){
+		printf("distance cannot be negative!");
+		return 0;
 	}
-
-	printf("\n%dfeet %.2finches",x,y);
-	return 0;
+	printf("enter inches:");
+	scanf("%f",&s[i].inches);
+	if (s[i].inches<0){
+		printf("distance cannot be negative!"); }
+	}
+	
+	totalfeet=s[0].feet+s[1].feet;
+	totalinches=s[0].inches+s[1].inches;
+	printf("%.2f\n",totalinches);
+	
+if (totalinches >= 12) {
+    totalfeet += totalinches / 12;  
+    totalinches=fmod(totalinches,12);           
+}
+	printf("total distance:%dfeets and %.2finches",totalfeet,totalinches);
+		return 0;
+	
 }
